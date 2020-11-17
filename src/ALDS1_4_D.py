f8af1cbfd4987503762fb328b81b916b914f666a
potof <slptmac@gmail.com>
2020-11-18 00:04:45 +0900

    AC !! めちゃくちゃ難しかった・・・・

--- src/ALDS1_4_D.py
+++ src/ALDS1_4_D.py
@@ -5,27 +5,36 @@ n, k = map(int, stdin.readline().split())
 
 baggages = [int(stdin.readline()) for _ in range(n)]
 
-trucks = [0 for _ in range(k)]
-truck_num = 0
-search_flg = True
-
-max_baggage = math.ceil(sum(baggages) / k)
-
-while search_flg:
-
-    # 荷物を足し合わせる
+def get_baggae_num(baggages, weight, truck_num):
+    trucks = [0 for _ in range(truck_num)]
+    truck_num = 0
+    count = 0
     for index, item in enumerate(baggages):
         trucks[truck_num] += item
-        if index != n-1 and trucks[truck_num] + baggages[index+1] > max_baggage:
+
+        if trucks[truck_num] > weight:
+            break
+
+        if index != n-1 and trucks[truck_num] + baggages[index+1] > weight:
             if truck_num < k-1:
                 truck_num += 1
             else:
-                truck_num = 0
-                trucks = [0 for _ in range(k)]
-                max_baggage += 1
-                search_flg = True
                 break
-        search_flg = False
+        count += 1
+    return count
 
-# print(trucks)
-print(max(trucks))
+
+max_wight = 0
+left = 0
+right = n * 10000
+mid = (left + right) // 2
+while left <= right:
+    baggage_num = get_baggae_num(baggages, mid, k)
+    if baggage_num == n:
+        max_wight = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+    mid = (left + right) // 2
+
+print(max_wight)
