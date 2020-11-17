72995690d790c3220790e80780f95557ed971b7d
potof <slptmac@gmail.com>
2020-11-17 22:42:42 +0900

    TLE になった・・・

--- src/ALDS1_4_D.py
+++ src/ALDS1_4_D.py
@@ -1,31 +1,31 @@
+import math
 from sys import stdin
 
 n, k = map(int, stdin.readline().split())
 
 baggages = [int(stdin.readline()) for _ in range(n)]
-baggages.sort(reverse=True)
 
 trucks = [0 for _ in range(k)]
 truck_num = 0
-turn = True
-for index, baggage in enumerate(baggages):
-    trucks[truck_num] += baggage
-    if turn:
-        if truck_num == k-1:
-            turn = False
-        elif index != n-1:
-            tmp_truck = trucks[truck_num] + baggages[index + 1]
-            next_truck = trucks[truck_num + 1]
-            if tmp_truck > next_truck:
+search_flg = True
+
+max_baggage = math.ceil(sum(baggages) / k)
+
+while search_flg:
+
+    # 荷物を足し合わせる
+    for index, item in enumerate(baggages):
+        trucks[truck_num] += item
+        if index != n-1 and trucks[truck_num] + baggages[index+1] > max_baggage:
+            if truck_num < k-1:
                 truck_num += 1
-    else:
-        if truck_num == 0:
-            turn = True
-        elif index != n-1:
-            tmp_truck = trucks[truck_num] + baggages[index + 1]
-            next_truck = trucks[truck_num - 1]
-            if tmp_truck > next_truck:
-                truck_num -= 1
+            else:
+                truck_num = 0
+                trucks = [0 for _ in range(k)]
+                max_baggage += 1
+                search_flg = True
+                break
+        search_flg = False
 
 # print(trucks)
 print(max(trucks))
